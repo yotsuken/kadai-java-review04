57bd7c2102884098853141eb61df51b26658d573
yotsuken <yotsumoto.ken@gmail.com>
2023-02-10 21:04:21 +0900

    レッスン７

---
+++ src/animal/Human.java
@@ -0,0 +1,37 @@
+package animal;
+
+public class Human extends Animal implements Thinkable {
+
+    //フィールド
+    private String hobby; //趣味
+    
+    
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+
+    public void think() {
+        
+        System.out.println("私は" + this.hobby + "について考えています。");
+
+    }
+
+  //コンストラクタ
+    public Human() {
+        
+    }
+
+    //引数ありのコントラクタ
+    public Human(String name, int age, String hobby) {
+        super(name,age);
+        this.hobby = hobby;
+    }
+
+
+
+}
